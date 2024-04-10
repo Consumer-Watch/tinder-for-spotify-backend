87cd808d4dc31bab4cebce074ff890573c9e76f5
fortune710 <fortune0208@yahoo.com>
2024-04-10 10:32:17 -0400

    created success and error responses for API

---
+++ utils/responses.py
@@ -0,0 +1,16 @@
+from typing import Any, int
+from flask import jsonify
+
+def success_response(data: Any, status_code: int = 200):
+    return jsonify({
+        "success": True,
+        "message": "OK",
+        "data": data
+    }), status_code
+
+def error_response(status_code: int = 500, message: str = "An error occured"):
+    return jsonify({
+        "success": False,
+        "message": message,
+        "data": None
+    }), status_code
