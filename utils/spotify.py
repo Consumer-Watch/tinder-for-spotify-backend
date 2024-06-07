1944197fc384244bf8b732aa41e266b1ff563b8d
fortune710 <fortune0208@yahoo.com>
2024-06-06 20:31:04 -0400

    configured spotify api query to return neccesary data

--- utils/spotify.py
+++ utils/spotify.py
@@ -16,10 +16,34 @@ def get_top_items_from_api(access_token: str, type: str) -> list[dict[str, any]]
     url = f"https://api.spotify.com/v1/me/top/{type}"
     response = requests.get(url=url, params=params, headers=headers)
     top_items = response.json()
-    print(top_items, "items")
     top_items = top_items["items"]
 
     #change this from top_artists
-    top_artists = [{"name": item["name"], "image": item['images'][-1]['url'] if item['images'] else None, "id": item["id"], "position": index+1 } for index, item in enumerate(top_items)]
-    return top_artists
+    if type == "artists":
+        items = [{
+            "name": item["name"], 
+            "image": item['images'][-1]['url'] if item['images'] else None, 
+            "id": item["id"], 
+            "position": index + 1,
+
+        } for index, item in enumerate(top_items)]
+    
+    elif type == "tracks":
+        items = [{
+            "name": item["name"], 
+            "image": item['album']['images'][1]['url'] if item['album']['images'] else None, 
+            "id": item["id"], 
+            "position": index+1,
+            "preview_url": item["preview_url"],
+            "artists": [{ 
+                "id": artist["id"], 
+                "name": artist["name"],
+                "image": ""
+            } for artist in item["artists"]] if item["artists"] else None
+
+        } for index, item in enumerate(top_items)]
+    else:
+        items = []        
+    
+    return items
 
