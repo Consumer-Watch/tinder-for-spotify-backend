3d4f7ff90c82c1209e38d0d48919e9870ef39934
fortune710 <fortune0208@yahoo.com>
2024-06-06 20:29:04 -0400

    controllers to handle top tracks and artists from spotify

---
+++ controllers/usertoptracks.py
@@ -0,0 +1,44 @@
+from models.usertoptracks import UserTopTracks
+from utils.functions import generate_random_id
+from config.database import db
+from utils.spotify import get_top_items_from_api as top_tracks
+
+
+def create_top_track(user_id: str, access_token: str):
+    existing_top_track = UserTopTracks.query.filter_by(
+        user_id = user_id,
+    ).one_or_none()
+
+    if existing_top_track is None:
+        top_items = top_tracks(
+            type = "tracks",
+            access_token = access_token
+        )
+
+        tracks = { "data" : top_items }
+
+        new_id = generate_random_id(15)
+        new_top_track = UserTopTracks(
+            id = new_id,
+            user_id = user_id,  
+            tracks = tracks
+        )
+
+        db.session.add(new_top_track)
+        db.session.commit()
+        return new_top_track.toDict()["tracks"]
+    
+    return existing_top_track.toDict()["tracks"]
+    
+
+def delete_top_tracks(user_id: str):
+    UserTopTracks.query.filter_by(user_id = user_id).delete()
+    db.session.commit()
+
+def get_top_tracks(user_id: str):
+    top_tracks = UserTopTracks.query.filter_by(user_id = user_id).one_or_none()
+
+    if top_tracks is None:
+        return { "data": [] }
+
+    return top_tracks.toDict()["tracks"]    
--- models/usertopartists.py
+++ models/usertopartists.py
@@ -1,15 +1,20 @@
 from config.database import db
 from sqlalchemy import inspect
-from datetime import datetime
+from sqlalchemy.dialects.postgresql import JSONB
+from datetime import datetime, timedelta
+
+DEFAULT_UPDATE_INTERVAL_DAYS = 30
+DEFAULT_UPDATE = datetime.today() + timedelta(days = DEFAULT_UPDATE_INTERVAL_DAYS)
 
 class UserTopArtists(db.Model):
     __tablename__ = "users-top-artists"
 
     id = db.Column(db.String(15), primary_key=True)
     user_id = db.Column(db.String(), db.ForeignKey('users.id'), index=True)
-    artist_id = db.Column(db.String(), db.ForeignKey('artists.id'), index=True)
-    position_for_user = db.Column(db.Integer)
-    created_at = db.Column(db.DateTime, default=datetime.utcnow)
+    #artist_id = db.Column(db.String(), db.ForeignKey('artists.id'), index=True)
+    #position_for_user = db.Column(db.Integer)
+    artists = db.Column(JSONB)
+    next_update = db.Column(db.DateTime, default=DEFAULT_UPDATE)
 
     def toDict(self):
         return { c.key: getattr(self, c.key) for c in inspect(self).mapper.column_attrs }
