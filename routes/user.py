9a0f0e46273aff8cbcf24ca34804eb1adf844526
fortune710 <fortune0208@yahoo.com>
2024-06-06 20:29:24 -0400

    refactoed routes

--- routes/user.py
+++ routes/user.py
@@ -2,13 +2,11 @@ from config.app import app
 from flask import request
 from controllers.user import get_user
 from controllers.usertopartists import get_top_artists
-from utils.responses import success_response
+from utils.responses import error_response, success_response
 from utils.spotify import get_top_items_from_api
 
-from controllers.artists import create_artist
 from controllers.usertopartists import create_top_artist
-from config.redis import id_cache
-import asyncio
+from controllers.usertoptracks import create_top_track, get_top_tracks
 
 @app.route('/users/<id>', methods=["GET", "PUT", "DELETE"])
 def users_route(id: str):
@@ -18,27 +16,34 @@ def users_route(id: str):
 @app.route('/users/<id>/top/<type>')
 def user_top_items(id: str, type: str):
     if type == 'artists':
-        return get_top_artists(id)
+        artists = get_top_artists(id)
+        return success_response(artists["data"], 200)
+    elif type == 'tracks':
+        tracks = get_top_tracks(id)
+        return success_response(tracks["data"], 200)
     else:
-        pass
+        return error_response(400, "Invalid Value for Type")
 
     
-@app.route('/users/me/top/<type>', methods=["POST", "GET"])
+@app.route('/users/me/top/<type>', methods=["POST"])
 def get_top_items(type: str):
+    if request.method == "GET":
+        return error_response(405, "Method not allowed")
+    
     access_token = request.get_json().get('access_token', None);
     user_id = request.get_json().get('user_id', None);
 
-    print("user_id", user_id)
-    top_items = get_top_items_from_api(
-        type = type,
-        access_token = access_token
-    )
-    
-    if type == "artists":
-        for artist in top_items:
-            create_artist(artist)
-            create_top_artist(user_id=user_id, artist_id=artist["id"], position=artist["position"])
-        return success_response(top_items)
+    try:
+        
+        if type == "artists":
+            top_items = create_top_artist(user_id, access_token)
+            
+        elif type == "tracks":
+            top_items = create_top_track(user_id, access_token)
+            
+        else:
+            return error_response(400, "Invalid Selection")
 
-    else:
-        pass
+        return success_response(top_items["data"], 201)
+    except Exception as e:
+        return error_response(500, str(e))
