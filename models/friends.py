2f4a380c4279e280d02fda775f00f728fedc58b4
fortune710 <fortune0208@yahoo.com>
2024-06-18 11:56:59 -0400

    created friends table

---
+++ models/friends.py
@@ -0,0 +1,23 @@
+from config.database import db
+from sqlalchemy import inspect
+from datetime import datetime
+from enum import Enum
+
+class FriendRequestStatus(Enum):
+    pending = "pending"
+    accepted = "accepted"
+    rejeted = "rejected"
+
+class FriendRequests(db.Model):
+    __tablename__ = "friend-requests"
+    
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.String(50), db.ForeignKey('users.id'), index=True)
+    friend_id = db.Column(db.String(50), db.ForeignKey('users.id'), index=True)
+    status = db.Column(db.Enum(FriendRequestStatus))
+    created_at = db.Column(db.DateTime, default=datetime.utcnow)
+    updated_at = db.Column(db.DateTime, default=datetime.utcnow)
+
+    def toDict(self):
+        return { c.key: getattr(self, c.key) for c in inspect(self).mapper.column_attrs }
+
