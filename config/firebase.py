0a0affa93c864b7c42be92fb64b29b84219c9f15
fortune710 <fortune0208@yahoo.com>
2024-07-12 13:58:40 -0400

    firebase config

--- config/firebase.py
+++ config/firebase.py
@@ -1,6 +1,20 @@
 import firebase_admin
 from firebase_admin import credentials
+from os import getenv
 
 
 app_options = {'projectId': 'sonder-63ac2'}
-default_app = firebase_admin.initialize_app(options=app_options)
+
+def get_credentials():
+    if getenv("GCP_PRIVATE_KEY"):
+        return {
+            "credentials": {
+                "client_email": getenv("GCP_SERVICE_ACCOUNT_EMAIL"),
+                "private_key": getenv("GCP_PRIVATE_KEY"),
+            },
+            'projectId': 'sonder-63ac2'
+        }
+    else:
+        return None
+    
+default_app = firebase_admin.initialize_app(credential=get_credentials())
