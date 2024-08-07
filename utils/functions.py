bca552df9db8e987f7d55490de6cfdcf96ec3d19
fortune710 <fortune0208@yahoo.com>
2024-08-07 16:08:49 -0400

    aadded function to parse date

--- utils/functions.py
+++ utils/functions.py
@@ -16,6 +16,24 @@ def get_future_date(start_date: datetime, days: int = 30):
     DEFAULT_UPDATE = start_date + timedelta(days = DEFAULT_UPDATE_INTERVAL_DAYS)
     return DEFAULT_UPDATE
 
+def get_month_and_year(datetime_string):
+  """Extracts month and year from a datetime string.
+
+  Args:
+      datetime_string: The datetime string in the format 'YYYY-MM-DDTHH:MM:SS.ffffff'.
+
+  Returns:
+      A tuple containing the month and year as strings.
+  """
+
+  # Parse the datetime string into a datetime object
+  datetime_obj = datetime.strptime(datetime_string, '%Y-%m-%dT%H:%M:%S.%f')
+
+  # Extract month and year
+  month = datetime_obj.strftime('%B')  # Month in full text format
+  year = str(datetime_obj.year)
+
+  return month + " " + year
 
 def send_email_to_user(subject: str, body: str, recipents: List[str]):
     msg = Message(
