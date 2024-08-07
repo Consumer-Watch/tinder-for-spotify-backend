65d3cdea308a05b607d1efbecee5dc5d6b4d181b
fortune710 <fortune0208@yahoo.com>
2024-08-07 14:02:07 -0400

    set up prod and dev urls

--- config/database.py
+++ config/database.py
@@ -1,13 +1,20 @@
 import os
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
+from sqlalchemy import text
 from config.app import app
 
+DATABASE_URL = {
+    "prod": os.getenv("DATABASE_PROD_URL"),
+    "dev": os.getenv("DATABASE_URL")
+}
 
 
-app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv("DATABASE_URL")
+app.config['SQLALCHEMY_DATABASE_URI'] = DATABASE_URL[os.getenv("ENV")]
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 
 db = SQLAlchemy(app)
+
+#db.session.execute(text('CREATE EXTENSION IF NOT EXISTS vector'))
 migrate = Migrate(app, db)
